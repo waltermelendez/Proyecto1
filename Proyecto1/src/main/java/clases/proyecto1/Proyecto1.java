5df26ab63ed0e24370b0d993fe03389f03f29999
Walter Meléndez <melendez@gmail.com>
2025-03-19 20:57:57 -0600

    Inicio 1

---
+++ Proyecto1/src/main/java/clases/proyecto1/Proyecto1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package clases.proyecto1;
+
+/**
+ *
+ * @author walte
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
